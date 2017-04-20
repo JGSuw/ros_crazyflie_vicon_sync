a5d976a59b9fd5d6c744a540f2f8a9f0e370f6b2
Joseph Sullivan <jgs6156@uw.edu>
2017-04-19 17:46:32 -0700

    Added services to begin and end data trials (writing data to disk)

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import sys
+import os
 import struct
 import logging
+import time
+import datetime
 import rospy
 
 # import crazyflie-lib-python packages
@@ -10,6 +13,7 @@ from cflib.crtp import crtpstack
 from cflib.crazyflie.log import LogConfig
 
 from cf_vicon_sync.srv import *
+from vicon_bridge.msg import Marker
 
 logging.basicConfig()
 
@@ -24,10 +28,20 @@ class Node :
 
         rospy.init_node( 'cf_vicon_sync' )
 
+        self.output_directory = None
+
+        self.output_files = {}
+
+
         self.crazyflie = cflib.crazyflie.Crazyflie()
 
         # Advertize services
-        self.get_interfaces_srv = rsopy.Service ( 'get_interfaces', GetInterfaces, self.get_interfaces )
+
+        self.begin_trial_srv = rospy.Service ( 'begin_trial', BeginTrial, self.begin_trial )
+
+        self.end_trial_srv = rospy.Service ( 'end_trial', EndTrial, self.end_trial )
+
+        self.get_interfaces_srv = rospy.Service ( 'get_interfaces', GetInterfaces, self.get_interfaces )
 
         self.connect_srv = rospy.Service ( 'connect', Connect, self.connect )
 
@@ -37,45 +51,109 @@ class Node :
 
         self.flash_ir_led = rospy.Service ( 'flash_ir_led', FlashIrLed, self.flash_ir_led )
 
-        self.crazyflie.add_port_callback ( GCS_SYNC_PORT, self.handle_packet )
-
         # Subscribe to topics
-        self.marker_sub = rospy.Subscriber( 'vicon/markers', Markers, self.marker_callback )
+        self.marker_sub = rospy.Subscriber( 'vicon/marker', Marker, self.marker_callback )
 
+        self.crazyflie.add_port_callback ( GCS_SYNC_PORT, self.crtp_callback )
+
+
+
+    """
+    Begins a new trial (records incoming data to files), at the specified directory.
+    """
+
+    def begin_trial ( self, request ) :
+
+        if self.output_directory is not None :
+            return "trial is already in progress."
+
+        else :
+            self.output_directory = request.directory
+            now = datetime.date.fromtimestamp( time.time() )
+            self.output_directory += '_' + str(now)
+
+            trial_count = 1
+
+            if os.path.exists(self.output_directory) :
+                for name in os.listdir(self.output_directory) :
+                    if name.rfind('trial') >= 0 :
+                        trial_count += 1
+
+            self.output_directory += '/trial_{}'.format(trial_count)
+
+            try :
+                os.makedirs ( self.output_directory )
+                os.chdir ( self.output_directory )
+                return "ok, trial started at {}".format( self.output_directory )
+
+            except OSError as e:
+                error_string = str(e)
+                raise ( rospy.ServiceException(error_string) )
+
+
+
+    """
+    Closes all open data files to end this trial
+    """
+
+    def end_trial ( self, request ) :
+
+        if self.output_directory is None :
+            return "no trial in progress."
+
+        else :
+            for filename in self.output_files.keys() :
+                self.output_files[filename].close()
+                self.output_files.pop(filename)
+            self.output_files = None
+            return "ok, trial finished"
+
+
+    """
+    Returns a stringified list of available CRTP interfaces
+    """
+    def get_interfaces ( self, request ) :
+        interfaces = cflib.crtp.scan_interfaces( None )
+        return str(interfaces)
 
     """
     Opens an indexed link interface to a crazyflie
     """
     def connect ( self, request ) :
 
-        if self.cf.connected_ts is not None:
+        if self.crazyflie.connected_ts is not None:
             return "already connected to a crazyflie"
 
         else :
             interfaces = cflib.crtp.scan_interfaces( None )
             try:
                 interface = interfaces[request.index].pop(0)
-                self.cf.connect(interface)
+                self.crazyflie.open_link(interface)
+                return "ok"
             except IndexError as e :
-                exc = rospy.ServiceException("Service could not process request: " + str(e) )
+                exc = rospy.ServiceException( str(e) )
                 raise(exc)
+
+
 
     """
     Closes an open link to a crazyflie
     """
-    def disconnect ( self ) :
+    def disconnect ( self, request ) :
 
-        if self.cf.connected_ts is not None :
-            self.cf.close_link()
+        if self.crazyflie.connected_ts is not None :
+            self.crazyflie.close_link()
             return "ok, disconnected"
         else : return "nothing to disconnect"
+
+
 
     """
     Sets the state of the active marker on the crazyflie to on or off
     """
     def toggle_ir_led ( self, request ) :
 
-        if self.cf.connected_ts is not None :
+        if self.crazyflie.connected_ts is not None :
 
             packet = crtpstack.CRTPPacket()
             packet._set_port ( GCS_SYNC_PORT )
@@ -87,22 +165,21 @@ class Node :
         else : return "not connected"
 
 
+
     """
     Pulses the active marker on the crazyflie for 100ms every 1s, flash_count
     number of times.
     """
-    def flash_ir_led ( self, flash_count ) :
+    def flash_ir_led ( self, request ) :
 
-        if self.cf.connected_ts is not None :
+        if self.crazyflie.connected_ts is not None :
 
             for i in range ( request.flash_count ):
 
                 packet = crtpstack.CRTPPacket()
                 packet._set_port ( GCS_SYNC_PORT )
                 packet._set_channel ( TIMESTAMP_REQUEST_CHANNEL )
-                packet.set_channel ( LED_TOGGLE_CHANNEL )
-
-                timestamp = rospy.get_time()
+                timestamp = rospy.get_rostime()
 
                 packet._set_data ( struct.pack('ii', timestamp.secs, timestamp.nsecs) )
 
@@ -114,17 +191,53 @@ class Node :
 
         else : return "not connected"
 
+
+
+    """
+    Handles marker messages, writing the data for each marker to its own file
+    """
+    def marker_callback ( self, data ) :
+        filename = "{}_{}.csv".format( data.subject_name, data.marker_name )
+
+
+        seconds = data.header.stamp.seconds + data.header.stamp.nanoseconds / 1e9
+
+        line = "{},{},{},{}\n".format( data.x,
+                                     data.y,
+                                     data.z,
+                                     seconds )
+
+        if self.output_directory is not None :
+            if filename not in self.output_files.keys() :
+                self.output_files[filename] = open ( filename, 'w')
+
+            self.output_files[filename].write( line )
+
+
+
+    """
+    Handles packets from the crazyflie, writing timestamps to a file
+    """
+    def crtp_callback ( self, packet ) :
+
+        print("incoming packet!")
+
+        ( seconds, nseconds, cfstamp ) = struct.unpack ( 'iii',
+                                                   packet._get_data() )
+
+        posixstamp = seconds + nseconds / 1e9
+
+        line = "{},{}\n".format( posixstamp, cfstamp )
+
+        if self.output_files is not None :
+            if 'cf_timestamps.csv' not in self.output_files.keys() :
+                self.output_files['cf_timestamps.csv'] = open ( 'cf_timestamps.csv', 'w' )
+            self.output_files['cf_timestamps.csv'].write( line )
+
+
+
 if __name__ == "__main__" :
 
     cflib.crtp.init_drivers ( enable_debug_driver = False )
-    # Connect to the crazyflie
-    interfaces = cflib.crtp.scan_interfaces(None)
-
-    if len(interfaces) > 0 :
-        link = interfaces[0][0]
-        crazyflie = cflib.crazyflie.Crazyflie()
-        crazyflie.open_link ( interfaces[0][0] )
-
-        node = Node ( crazyflie )
-
-        rospy.spin()
+    node = Node ()
+    rospy.spin()
